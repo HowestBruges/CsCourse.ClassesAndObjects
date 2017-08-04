80f43c98c226f0a9c8f0431eba16aedff2b7f9aa
Bart Soete <soeteba@hotmail.com>
2017-08-04 13:53:21 +0200

    Added ClassLibrary, Class and Reference

--- CsCourse.ClassesAndObjects.WPF/MainWindow.xaml.cs
+++ CsCourse.ClassesAndObjects.WPF/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using CsCourse.ClassesAndObjectsLib;
 
 namespace CsCourse.ClassesAndObjects.WPF
 {
---
+++ CsCourse.ClassesAndObjectsLib/Car.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsCourse.ClassesAndObjectsLib
+{
+    public enum Brand { BMW, Volkswagen, Ford, Mercedes}
+
+    public class Car
+    {
+        // Fields
+        public int maxSpeed;
+        public int actSpeed;
+        public Brand carBrand;
+
+        // Constructor
+        public Car()
+        {
+
+        }
+
+        // Constructor Overload
+        public Car(Brand carBrand, int maxSpeed)
+        {
+            this.carBrand = carBrand;
+            this.maxSpeed = maxSpeed;
+        }
+
+
+
+    }
+}
